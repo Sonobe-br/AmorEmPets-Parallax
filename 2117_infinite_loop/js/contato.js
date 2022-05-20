874784259e44611d799b079fed815ca7a32f104e
Daniel-TI <daniel.silveira@indbandeirante.com.br>
2022-05-20 14:44:26 -0300

    ajustando a sessão contato

---
+++ 2117_infinite_loop/js/contato.js
@@ -0,0 +1,16 @@
+function Enviar() {
+
+    let nome = document.getElementById ("IdName");
+    if (nome.value != "") {
+        alert('Obrigado Sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
+
+
+/*  let enviar = document.getElementById ("Idsubmit");
+ if (enviar.value != "") {
+
+     alert('Obrigado Sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+
+ } */
